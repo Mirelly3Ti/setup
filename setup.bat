0cbadc1b3befb083f53fd07bb2afa72078b34da4
Mirelly3Ti <mihsato8@gmail.com>
2024-05-07 09:32:15 -0300

    fix

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 git config --global user.name "Mirelly3Ti"
 git config --global user.email "mihsato8@gmail.com" 
 
-cd C:\Users\MirellySilvaSato\setup>
+cd C:\Users\MirellySilvaSato\setup
 
 git clone https://github.com/Mirelly3Ti/Caeser-Cipher.git 
 cd Caeser-Cipher 
